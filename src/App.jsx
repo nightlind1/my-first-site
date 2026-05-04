b07192ef813ea6d62410497a85e12dc72fd33922
Сергей Крищик <nightlind.s@gmail.com>
2026-05-04 21:57:55 +0300

    Первый запуск сайта

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Мой первый сайт</h1>
+      <p>Я учусь создавать сайты с помощью ChatGPT и VS Code.</p>
+      <button>Начать</button>
+    </div>
+  )
+}
+
+export default App
